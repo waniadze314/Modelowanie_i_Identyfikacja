4af05f64407705611ef9a308d598f69bcf777af3
waniadze314 <kamilkasjan@poczta.fm>
2020-05-15 22:31:05 +0200

    add orthogonal estimator

--- core_triangle.m
+++ core_triangle.m
@@ -2,10 +2,10 @@ function value = core_triangle(argument)
 size = length(argument);
 value = zeros(1,size);
 for n=1:size
-    if(argument(n)<=0 && argument(n)>-1)
-        value(n) = 1+argument(n);
-    elseif(argument(n)>0 && argument(n)<1)
-        value(n) = 1-argument(n);
+    if(argument(n)<=0 && argument(n)>-0.5)
+        value(n) = 1+2*argument(n);
+    elseif(argument(n)>0 && argument(n)<0.5)
+        value(n) = 1-2*argument(n);
     else 
         value(n) = 0;     
     end
--- estimator_NW.m
+++ estimator_NW.m
@@ -2,15 +2,15 @@ function values = estimator_NW(probes1, probes2)
 x1 = min(probes1);
 x2 = max(probes1);
 size = length(probes1);
-h = 0.45;
+h = 0.2;
 x = linspace(x1,x2,size);
 values = zeros(size,1);
 for i=1:size
     up_sum = 0;
     down_sum = 0;
     for j=1:size
-        up_sum = up_sum + probes2(j).*core_keys((probes1(j)-x(i))/h);
-        down_sum = down_sum + core_keys((probes1(j)-x(i))/h);        
+        up_sum = up_sum + probes2(j).*core_rectangle((probes1(j)-x(i))/h);
+        down_sum = down_sum + core_rectangle((probes1(j)-x(i))/h);        
     end
     values(i) = up_sum/down_sum;
 end
